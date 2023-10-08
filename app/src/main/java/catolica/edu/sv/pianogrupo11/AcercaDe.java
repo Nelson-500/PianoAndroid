be147fb26b58d99e47e08a11206911700e5a2c59
Nelson-500 <nel.lopez500@gmail.com>
2023-10-07 22:25:49 -0600

    ajustes para el primer funcionamiento de la app

---
+++ app/src/main/java/catolica/edu/sv/pianogrupo11/AcercaDe.java
@@ -0,0 +1,13 @@
+package catolica.edu.sv.pianogrupo11;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+    public class AcercaDe extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_acercade);
+        }
+    }
--- app/src/main/java/catolica/edu/sv/pianogrupo11/MainActivity.java
+++ app/src/main/java/catolica/edu/sv/pianogrupo11/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,35 +13,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_piano_tradiconal);
+        setContentView(R.layout.activity_main);
     }
-    public void TipodePiano(View view){
-        //cuadro de dialogo selectivo entre piano tradicional, piano infantil de la selva  y piano de instrumentos musicales
-       final String[] items = {"Piano Tradicional", "Piano Infantil de la Selva", "Piano de Instrumentos Musicales"};
+    public void TipodePiano(View view) {
+        final String[] items = {"Piano Tradicional", "Piano Infantil de la Selva", "Piano de Instrumentos Musicales"};
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Seleccione el tipo de piano");
-        builder.setItems(items,new DialogInterface.OnClickListener(){
+        builder.setItems(items, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int i) {
-                if (items[i].equals("Piano Tradicional")){
-                    //llamada a la actividad piano tradicional
-                    setContentView(R.layout.activity_piano_tradiconal);
-                }
-                else if (items[i].equals("Piano Infantil de la Selva")){
-                    //llamada a la actividad piano infantil de la selva
-                    setContentView(R.layout.activity_piano_infantil_de_la_selva);
-                }
-                else if (items[i].equals("Piano de Instrumentos Musicales")){
-                    //llamada a la actividad piano de instrumentos musicales
-                    setContentView(R.layout.activity_piano_de_instrumentos_musicales);
+                if (items[i].equals("Piano Tradicional")) {
+                    // Llamada a la actividad piano tradicional usando Intent
+                    Intent intent = new Intent(MainActivity.this, pianoTradicional.class);
+                    startActivity(intent);
+
+                } else if (items[i].equals("Piano Infantil de la Selva")) {
+                    // Llamada a la actividad piano infantil de la selva usando Intente
+
+                    Intent intent = new Intent(MainActivity.this, piano_infantil_de_la_selva.class);
+                    startActivity(intent);
+
+                } else if (items[i].equals("Piano de Instrumentos Musicales")) {
+                    // Llamada a la actividad piano de instrumentos musicales usando Intente
+                    Intent intent = new Intent(MainActivity.this, piano_de_instrumentos_musicales.class);
+                    startActivity(intent);
                 }
             }
         });
 
+        // Mostrar el cuadro de diálogo
+        builder.show();
     }
+
     public void Acercade(View view){
-        //muestra el layout de activity_acercade
-        setContentView(R.layout.activity_acercade);
+        //muestra el layout de activity_acercade con Intente
+        Intent intent = new Intent(this, AcercaDe.class);
+        startActivity(intent);
     }
     public void Salir(View view){
         //finaliza la aplicacion
