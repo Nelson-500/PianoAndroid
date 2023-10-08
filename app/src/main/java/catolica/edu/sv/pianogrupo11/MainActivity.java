99aa34fad1335497e7239e9775022b9810fc5025
Nelson-500 <nel.lopez500@gmail.com>
2023-10-07 19:50:43 -0600

    creacion de los 3 metodos para la pantalla del menú

--- app/src/main/java/catolica/edu/sv/pianogrupo11/MainActivity.java
+++ app/src/main/java/catolica/edu/sv/pianogrupo11/MainActivity.java
@@ -2,7 +2,10 @@ package catolica.edu.sv.pianogrupo11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void TipodePiano(View view){
+        //cuadro de dialogo selectivo entre piano tradicional, piano infantil de la selva  y piano de instrumentos musicales
+       final String[] items = {"Piano Tradicional", "Piano Infantil de la Selva", "Piano de Instrumentos Musicales"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Seleccione el tipo de piano");
+        builder.setItems(items,new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                if (items[i].equals("Piano Tradicional")){
+                    //llamada a la actividad piano tradicional
+                    setContentView(R.layout.activity_piano_tradicional);
+                }
+                else if (items[i].equals("Piano Infantil de la Selva")){
+                    //llamada a la actividad piano infantil de la selva
+                    setContentView(R.layout.activity_piano_infantil_de_la_selva);
+                }
+                else if (items[i].equals("Piano de Instrumentos Musicales")){
+                    //llamada a la actividad piano de instrumentos musicales
+                    setContentView(R.layout.activity_piano_de_instrumentos_musicales);
+                }
+            }
+        });
+
+    }
+    public void Acercade(View view){
+        //muestra el layout de activity_acercade
+        setContentView(R.layout.activity_acercade);
+    }
+    public void Salir(View view){
+        //finaliza la aplicacion
+        finish();
+    }
+
 }
