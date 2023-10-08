13937a8b8caccb7b0fde207f959248bc974e58cb
CarlosD <carlos.chinchilla1@catolica.edu.sv>
2023-10-07 22:07:11 -0600

    metodos

--- app/src/main/java/catolica/edu/sv/pianogrupo11/pianoTradicional.java
+++ app/src/main/java/catolica/edu/sv/pianogrupo11/pianoTradicional.java
@@ -13,39 +13,89 @@ public class pianoTradicional extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_piano_tradiconal);
     }
-    public void NotaDo(View v){
-        MediaPlayer mp = MediaPlayer.create(this, R.raw.doo);
-        mp.start();
-    }
 
-    public void NotaRe(View v){
-        MediaPlayer mp = MediaPlayer.create(this, R.raw.ree);
-        mp.start();
+    public void sonidoDoo(View view){
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.doo);
+        if (sonido.isPlaying()) {
+            sonido.stop();
+        }else{
+            try {
+                sonido.start();
+            }catch (IllegalStateException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
-    public void NotaMi(View v){
-        MediaPlayer mp = MediaPlayer.create(this, R.raw.mii);
-        mp.start();
+    public void sonidoFaa(View view){
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.faa);
+        if (sonido.isPlaying()) {
+            sonido.stop();
+        }else{
+            try {
+                sonido.start();
+            }catch (IllegalStateException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
-    public void NotaFa(View v){
-        MediaPlayer mp = MediaPlayer.create(this, R.raw.faa);
-        mp.start();
+    public void sonidoMii(View view){
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.mii);
+        if (sonido.isPlaying()) {
+            sonido.stop();
+        }else{
+            try {
+                sonido.start();
+            }catch (IllegalStateException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
-    public void NotaSol(View v){
-        MediaPlayer mp = MediaPlayer.create(this, R.raw.sool);
-        mp.start();
+    public void sonidoLaa(View view){
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.laa);
+        if (sonido.isPlaying()) {
+            sonido.stop();
+        }else{
+            try {
+                sonido.start();
+            }catch (IllegalStateException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
-    public void NotaLa(View v){
-        MediaPlayer mp = MediaPlayer.create(this, R.raw.laa);
-        mp.start();
+    public void sonidoRee(View view){
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.ree);
+        if (sonido.isPlaying()) {
+            sonido.stop();
+        }else{
+            try {
+                sonido.start();
+            }catch (IllegalStateException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
-    public void NotaSi(View v){
-        MediaPlayer mp = MediaPlayer.create(this, R.raw.sii);
-        mp.start();
+    public void sonidoSii(View view){
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.sii);
+        if (sonido.isPlaying()) {
+            sonido.stop();
+        }else{
+            try {
+                sonido.start();
+            }catch (IllegalStateException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
+    public void sonidoSool(View view){
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.sool);
+        if (sonido.isPlaying()) {
+            sonido.stop();
+        }else{
+            try {
+                sonido.start();
+            }catch (IllegalStateException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- app/src/main/java/catolica/edu/sv/pianogrupo11/piano_de_instrumentos_musicales.java
+++ app/src/main/java/catolica/edu/sv/pianogrupo11/piano_de_instrumentos_musicales.java
@@ -2,7 +2,9 @@ package catolica.edu.sv.pianogrupo11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class piano_de_instrumentos_musicales extends AppCompatActivity {
 
@@ -10,5 +12,58 @@ public class piano_de_instrumentos_musicales extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_piano_de_instrumentos_musicales);
+    }
+
+    public void sonidoFlauta(View view) {
+
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.flautaa);
+        if (sonido.isPlaying()) {
+            sonido.stop();
+        } else {
+            try {
+                sonido.start();
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void sonidoGuitarra(View view) {
+
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.guitarraa);
+        if (sonido.isPlaying()) {
+            sonido.stop();
+        } else {
+            try {
+                sonido.start();
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void sonidoTrompeta(View view) {
+
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.trompetaa);
+        if (sonido.isPlaying()) {
+            sonido.stop();
+        } else {
+            try {
+                sonido.start();
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void sonidoSaxofon(View view) {
+
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.saxofoon);
+        if (sonido.isPlaying()) {
+            sonido.stop();
+        } else {
+            try {
+                sonido.start();
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
